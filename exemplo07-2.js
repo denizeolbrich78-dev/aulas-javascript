61a2626dd1dca4e64a54a441f06a9b8e67ff8aec
DENIZE OLBRICH <denize_olbrich@estudante.sesisenai.org.br>
2026-09-14 22:26:42 -0300

    adicionando exemplos 04 até 07

---
+++ exemplo07-2.js
@@ -0,0 +1,18 @@
+const nota1 = 85;
+
+        switch(nota) {
+
+case "nota1"
+(nota >= 90) {
+  console.log('Conceito A')
+  break;
+}
+case "nota1"
+ (nota >= 80) {
+  console.log('Conceito B')
+   break;
+} case "nota1"
+{
+  console.log('Precisa melhorar')
+  break;
+}
